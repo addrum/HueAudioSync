378a0693ee0185b4f7e8c15dc2e914810d15f13b
Adam Short <adamshort1994@gmail.com>
2017-02-11 17:55:09 +0000

    stop recording on exit

--- HueAudioSync/Program.cs
+++ HueAudioSync/Program.cs
@@ -5,6 +5,8 @@ namespace HueAudioSync
 {
     internal static class Program
     {
+        private static LoopbackRecorder _recorder;
+
         /// <summary>
         /// The main entry point for the application.
         /// </summary>
@@ -14,8 +16,16 @@ namespace HueAudioSync
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             //Application.Run(new Form1());
-            LoopbackRecorder recorder = new LoopbackRecorder();
-            recorder.StartRecording();
+
+            AppDomain.CurrentDomain.ProcessExit += OnProcessExit;
+
+            _recorder = new LoopbackRecorder();
+            _recorder.StartRecording();
+        }
+
+        private static void OnProcessExit(object sender, EventArgs e)
+        {
+            _recorder.StopRecording();
         }
     }
 }
